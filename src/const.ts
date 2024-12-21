4ef1e4a519851ba187f3ced9324d5147d803ff82
reindeer002 <lsj000424@gmail.com>
2024-12-21 10:29:43 +0000

    refactor: erase path string in url

--- src/const.ts
+++ src/const.ts
@@ -1,8 +1,8 @@
 import {SoopAPIBaseUrls} from "./types"
 
 export const DEFAULT_BASE_URLS: SoopAPIBaseUrls = {
-    soopPlayerBaseUrl: 'https://live.sooplive.co.kr/afreeca/player_live_api.php',
-    soopBridgeBaseUrl: 'wss://bridge.sooplive.co.kr/Websocket'
+    soopPlayerBaseUrl: 'https://live.sooplive.co.kr',
+    soopBridgeBaseUrl: 'wss://bridge.sooplive.co.kr'
 }
 
 export const DEFAULT_USER_AGENT = "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2049.0 Safari/537.36"
